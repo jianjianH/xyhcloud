8ab7cf8a8ed37b9cd6193d12c57c7c3614aaf0a0
Jne <jnehuang@welove-inc.com>
2018-09-19 18:29:39 +0800

    1. 添加强制更新 2. 添加筹款来源

--- miniprogram/pages/donate/donate.js
+++ miniprogram/pages/donate/donate.js
@@ -55,6 +55,27 @@ Page({
         })
     },
 
+    onReady: function () {
+        const updateManager = wx.getUpdateManager()
+
+        updateManager.onCheckForUpdate(function (res) {
+            // 请求完新版本信息的回调
+        })
+
+        updateManager.onUpdateReady(function () {
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success: function (res) {
+                    if (res.confirm) {
+                        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                        updateManager.applyUpdate()
+                    }
+                }
+            })
+        })
+    },
+
     onShareAppMessage: function(res) {
         mta.Event.stat('donate', {
             'share': 'true'
